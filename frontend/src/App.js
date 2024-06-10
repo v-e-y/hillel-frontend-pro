15aca4ae1e7408820509884b98475d9f5e6cec77
v-e-y <v-e-y@outlook.com>
2024-06-10 22:13:13 +0300

    ProductPreview: impl page., ProductPreviewCard: impl CMP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import Base from './components/Base/Base';
 import Main from './components/Main/Main';
 import Navbar from './components/Navbar/Navbar';
-import Login from './pages/Login/Login';
+import ProductPreview from './pages/ProductPreview/ProductPreview';
 
 const App = () => {
     return (
@@ -9,7 +9,7 @@ const App = () => {
             <Base>
                 <Navbar />
                 <Main>
-                    <Login />
+                    <ProductPreview />
                 </Main>
             </Base>
         </div>
